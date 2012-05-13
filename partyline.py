6e5789d8d39e9e9bdaee2d71a95fb9e4dae5c594
Adam Greig <adam@adamgreig.com>
2012-05-13 16:41:51 +0100

    use redis for persistency

--- partyline.py
+++ partyline.py
@@ -1,24 +1,52 @@
 import os
+import sys
 import flask
+import redis
+import pymongo
+import urlparse
 
 from twilio import twiml
 from twilio.rest import TwilioRestClient
 
+def config(var):
+    try:
+        return os.environ[var]
+    except KeyError:
+        print "Could not find {0} in env, quitting.".format(var)
+        sys.exit(1)
+
+def setup_mongo():
+    uri = config('MONGOHQ_URI')
+    dbname = urlparse.urlparse(uri).path[1:]
+    con = pymongo.Connection(uri)
+    db = con[dbname]
+    return db
+
+def setup_redis():
+    uri = urlparse.urlparse(config('REDIS_URI'))
+    return redis.StrictRedis(host=uri.hostname, port=uri.port, db=0,
+            password=uri.password)
+
+r = setup_redis()
+db = setup_mongo()
 app = flask.Flask(__name__)
 twilio_client = TwilioRestClient()
 
-def conference_running():
+def check_conferences_active():
     return (
         len(twilio_client.conferences.list(
             friendly_name="selocpartyline", status="in-progress")) +
         len(twilio_client.conferences.list(
             friendly_name="selocpartyline", status="init"))) > 0
 
+def conference_running():
+    return r.get('conference_running')
+
 def call_others(initiator):
     twilio_client.calls.create(
-        to=os.environ['TWILIO_TROLL_NUMBER'],
-        from_=os.environ['TWILIO_FROM_NUMBER'],
-        url=None, application_sid="AP92a26b662590492fa99225404e5cb0c7")
+        to=config('TWILIO_TROLL_NUMBER'),
+        from_=config('TWILIO_FROM_NUMBER'),
+        url=None, application_sid=config('TWILIO_APP'))
 
 @app.route('/')
 def index():
@@ -28,12 +56,12 @@ def index():
 def call():
     r = twiml.Response()
     r.say("Welcome to the PARTY LINE.")
-    if not conference_running():
+    if r.get('conference_running'):
+        r.say("Connecting you to the party. Get ready to PARTY HARD.")
+    else:
+        r.set('conference_running', True)
         call_others(flask.request.args['From'])
         r.say("Phoning the rest of the party. Please wait.")
-    else:
-        r.say("Connecting you to the party. Get ready to PARTY HARD.")
-
     with r.dial() as d:
         d.conference("selocpartyline", muted=False, beep=True,
                 startConferenceOnEnter=True, endConferenceOnExit=False)
@@ -45,8 +73,14 @@ def call():
 def sms():
     return ""
 
+@app.route('/hangup')
+def hangup():
+    if not check_conferences_active():
+        r.set('conference_running', False)
+    return "OK"
+
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
     if os.environ.get('DEBUG', False):
         app.debug = True
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
