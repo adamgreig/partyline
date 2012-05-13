cd8769b04104a6a3df6df769abb97575bda25587
Adam Greig <adam@adamgreig.com>
2012-05-13 17:15:40 +0100

    use mongo

--- partyline.py
+++ partyline.py
@@ -39,14 +39,11 @@ def check_conferences_active():
         len(twilio_client.conferences.list(
             friendly_name="selocpartyline", status="init"))) > 0
 
-def conference_running():
-    return r.get('conference_running')
-
 def call_others(initiator):
-    twilio_client.calls.create(
-        to=config('TWILIO_TROLL_NUMBER'),
-        from_=config('TWILIO_FROM_NUMBER'),
-        url=None, application_sid=config('TWILIO_APP'))
+    for member in mongo_client.party_members.find():
+        twilio_client.calls.create(
+            to=member['number'], from_=config('TWILIO_FROM_NUMBER'), url=None,
+            application_sid=config('TWILIO_APP'))
 
 @flask_app.route('/')
 def index():
@@ -56,15 +53,22 @@ def index():
 def call():
     response = twiml.Response()
     response.say("Welcome to the PARTY LINE.")
-    if redis_client.exists('conference_running'):
-        response.say("Connecting you to the party. Get ready to PARTY HARD.")
+    number = flask.request.args.get('From', None)
+    if not mongo_client.party_members.find_one({'number': number}):
+        response.say("Sorry, only authorised party members may join the party.")
+        response.say("Goodbye.")
     else:
-        redis_client.set('conference_running', True)
-        call_others(flask.request.args['From'])
-        response.say("Phoning the rest of the party. Please wait.")
-    with response.dial() as dial:
-        dial.conference("selocpartyline", muted=False, beep=True,
-                startConferenceOnEnter=True, endConferenceOnExit=False)
+        if redis_client.exists('conference_running'):
+            response.say("Connecting you to the party.")
+            response.say("Get ready to PARTY HARD.")
+        else:
+            redis_client.set('conference_running', True)
+            call_others(flask.request.args['From'])
+            response.say("Phoning the rest of the party.")
+            response.say("Prepare to PARTY HARD.")
+        with response.dial() as dial:
+            dial.conference("selocpartyline", muted=False, beep=True,
+                    startConferenceOnEnter=True, endConferenceOnExit=False)
     response = flask.make_response(str(response))
     response.headers['Content-Type'] = 'application/xml'
     return response
